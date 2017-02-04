7c7bd36a94c88dfc0f7b0f86e4456221ac0a9159
Anton Boyko <boyko.ant@live.com>
2017-02-04 16:41:41 +0200

    form controller started

--- src/api/controllers/form-controller.js
+++ src/api/controllers/form-controller.js
@@ -4,11 +4,20 @@ var appService = require('./../../services/app-service');
 
 module.exports = {
 	getAllForms: function(req, res) {
-
+		appService.forms.getAllEntities().then(function(entities) {
+			res.json(entities);
+		});
 	},
 
 	getFormById: function(req, res) {
+		var id = req.swagger.params.id.value;
 
+		appService.forms.getEntityById(id)
+			.then(function(entity) {
+				res.json(entity);
+			}, function(error) {
+				res.status(error.code).end();
+			});
 	},
 
 	renderForm: function(req, res) {
